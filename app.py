efe66bed093281b6329e8cc7bcd3ce0ae4c39016
Saar Remez <saaremez@google.com>
2022-11-05 18:04:41 +0200

    j

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import telebot
 from utils import search_download_youtube_video
 from loguru import logger
+import time
+from yt_dlp import YoutubeDL
 
 
 class Bot:
@@ -55,12 +57,31 @@ class Bot:
 
 
 class QuoteBot(Bot):
-    def handle_message(self, message):
-        if message.text != 'Don\'t quote me please':
-            self.send_text_with_quote(message.text, message_id=message.message_id)
+    pass
 
 
 class YoutubeBot(Bot):
+    def search_download_youtube_video(video_name, num_results=1):
+        """
+        This function downloads the first num_results search results from Youtube
+        :param video_name: string of the video name
+        :param num_results: integer representing how many videos to download
+        :return: list of paths to your downloaded video files
+        """
+        results = []
+        with YoutubeDL() as ydl:
+            videos = ydl.extract_info(f"ytsearch{num_results}:{video_name}", download=True)['entries']
+
+            for video in videos:
+                results.append({
+                    'filename': ydl.prepare_filename(video),
+                    'video_id': video['id'],
+                    'title': video['title'],
+                    'url': video['webpage_url']
+                })
+
+        return results
+
     pass
 
 
